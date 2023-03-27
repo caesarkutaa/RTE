90ac6fda2e54aa971b0e07597b13f26ee015cfcc
caesarkutaa <caesarlouis0@gmail.com>
2023-03-27 23:59:27 +0100

    added admin routes,controller and model

---
+++ rte-server/routes/admin.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const {createAdmin,loginAdmin, updateAdmin, deleteAdmin} = require('../controllers/admin.controller');
+//const auth = require('../middleware/authentication');
+
+
+router.post('/register',createAdmin)
+router.post('/login', loginAdmin)
+router.put('/:id', updateAdmin)
+router.delete('/:id', deleteAdmin)
+
+
+
+module.exports = router;
