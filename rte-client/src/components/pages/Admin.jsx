d429afe50e8f8a94b24abdf9aa9eed10f1500f2b
techrook <mondayitohowo3@gmail.com>
2023-03-30 21:09:35 +0100

    fixed login

--- rte-client/src/components/pages/Admin.jsx
+++ rte-client/src/components/pages/Admin.jsx
@@ -1,17 +1,21 @@
 // import {React, useState} from 'react'
 import SideBar from '../partials/SideBar'
-import Login from '../login'
-
-
- 
-const tokenString = JSON.parse(localStorage.getItem('Token'))
-console.log(tokenString)
+import Login from '../login';
 
 const Admin = () => {
-  // const {token, settoken }= useToken()
-  if(!tokenString) {
-    return<Login  />
-  }
+
+  function setToken (){
+const tokenString = JSON.parse(localStorage.getItem('Token'))
+
+if(!tokenString) {
+  return<Login  />
+}
+}
+
+window.addEventListener("load", () => {
+  setToken()
+console.log(tokenString)
+});
 
   return (
     <div>
