3f7a81e5b3a6f98897c9b2f33db46e693a2f9ee1
techrook <mondayitohowo3@gmail.com>
2023-04-18 13:14:15 +0100

    homepage

--- rte-client/src/App.jsx
+++ rte-client/src/App.jsx
@@ -3,12 +3,14 @@ import Admin from './components/pages/Admin';
 import Login from './components/login';
 import { Routes, Route } from 'react-router-dom';
 import RequireAuth from "./components/RequireAuth";
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
     <Route path="login" element={<Login />} />
+    <Route path="/" element={<Home />} />
     <Route element={<RequireAuth />}>
       <Route path="admin" element={<Admin />} />
         </Route>
