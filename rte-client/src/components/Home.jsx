3d425b8c7615d403d91170f892d5891078843ba6
techrook <mondayitohowo3@gmail.com>
2023-04-29 20:57:59 +0100

    comittes

--- rte-client/src/components/Home.jsx
+++ rte-client/src/components/Home.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Sidebar from './partials/Sidebar'
+import Searchbar from './partials/Searchbar'
 const Home = () => {
   return (
     <>
+    
+    <div className='bg-slate-900'>
     <Sidebar />
-    </>
+    <Searchbar />
+    </div>
+    <div>
+    <h4>Latest Song</h4>
+    <div>
 
+    </div>
+    </div>
+    <div>
+    <h4>Latest Albums</h4>
+    <div>
+      
+    </div>
+    </div>
+    <div>
+    <h4>Latest News</h4>
+    <div>
+      
+    </div>
+    </div>
+</>
 
 
   )
